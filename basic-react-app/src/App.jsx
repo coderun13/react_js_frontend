deb0f731041ea8156f344da6716abe7d4e04d78a
coderun13 <snehalsingh.0407@gmail.com>
2024-04-24 23:21:12 +0530

    description

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -4,12 +4,17 @@ function Title() {
   return <h1>I am the Title!</h1>
 }
 
+function Description() {
+  return <h3>I am the description!</h3>
+}
+
 function App() {
   return (
     <div>
     <h1>This is my app component</h1>
     <p>inside app compoent we have: </p>
   <Title />
+  <Description />
   {/* <h1>Hello World</h1> */}
   </div>
   );
