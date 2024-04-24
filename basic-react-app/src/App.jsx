aff9881e2e517d16a554ed5f26e7cda89cec54b5
coderun13 <snehalsingh.0407@gmail.com>
2024-04-25 01:35:30 +0530

    use of js in file

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import Title from "./Title.jsx";
 function App() {
   return (
     <>
+    <p>2*2 = {2*2}</p>
         <Title />
         <Title />
     </>
--- basic-react-app/src/Title.jsx
+++ basic-react-app/src/Title.jsx
@@ -1,7 +1,9 @@
 
 function Title() {
+  let name = "Snehal";
   return (
     <div>
+      <h1>HI,{name.toUpperCase()}</h1>
     <h1>I am the Title!</h1>
     <h3>I am the description!</h3>
   </div>
