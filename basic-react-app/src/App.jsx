6b46b4bc940bf6fb68951713a0ed2f2d48701c05
coderun13 <snehalsingh.0407@gmail.com>
2024-04-24 23:56:24 +0530

    import export

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css"
 
-function Title() {
-  return <h1>I am the Title!</h1>
-}
+//import
+import Title from "./Title.jsx";
 
 function Description() {
   return <h3>I am the description!</h3>
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,9 @@
+
+function Title() {
+    return <h1>I am the Title!</h1>
+  }
+
+  //export
+export default Title;
+
+
