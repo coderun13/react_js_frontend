91d92cc5a0bf3e2a87b6b4d5a41ffe3816fb69c4
coderun13 <snehalsingh.0407@gmail.com>
2024-04-26 22:48:42 +0530

    props

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,9 +1,10 @@
 import "./Product.css";
 
-function Product(){
+function Product({title}){
+   // console.log(props.title);
     return(
         <div className = "Product">
-            <h3>Product Title</h3>
+            <h3>{title}</h3>
             <h5>Product Description</h5>
         </div>
     );
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product.jsx";
 function ProductTab(){
     return (
     <>
-    <Product />
-    <Product />
-    <Product />
+    <Product title="phone"/>
+    <Product title="laptop"/>
+    <Product title="pen"/>
     </>
     );
 }
