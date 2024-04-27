480f2c07c27a290688bea7fccd8dc4bf0a4bc043
coderun13 <snehalsingh.0407@gmail.com>
2024-04-27 19:06:50 +0530

    conditional by object

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -3,26 +3,16 @@ import "./Product.css";
 function Product({title, price,features}) {
     //const list = features.map((feature) => <li>{feature}</li>);
    // console.log(props.title);
-   if(price>=30000){
+   let isDiscount = price > 30000 ? "Discount of 5%" : "";
     return(
         <div className = "Product">
             <h3>{title}</h3>
             <h5>price: {price}</h5>
             <h5>Product Description</h5>
-            <p>Discount of 5%</p>
+            <p>{isDiscount}</p>
             <p>{features}</p>
         </div>
     );
-}else{
-    return(
-        <div className = "Product">
-            <h3>{title}</h3>
-            <h5>price: {price}</h5>
-            <h5>Product Description</h5>
-            <p>{features}</p>
-        </div>
-    );
-}
 }
 
 export default Product;
