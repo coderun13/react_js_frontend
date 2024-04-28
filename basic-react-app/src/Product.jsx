15c087eb1a5654396d7ca10b43db042668bfde83
coderun13 <snehalsingh.0407@gmail.com>
2024-04-28 14:22:17 +0530

    dynamic component styling

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -3,9 +3,10 @@ import "./Product.css";
 function Product({title, price,features}) {
     //const list = features.map((feature) => <li>{feature}</li>);
    // console.log(props.title);
+   let styles = {backgroundColor: price>3000? "pink" :""};
    let isDiscount = price > 30000 ? "Discount of 5%" : "";
     return(
-        <div className = "Product">
+        <div className = "Product" style= {styles}>
             <h3>{title}</h3>
             <h5>price: {price}</h5>
             <h5>Product Description</h5>
