676c2263db57c6b1683661002e4eda3d8c2f7801
coderun13 <snehalsingh.0407@gmail.com>
2024-04-28 23:14:40 +0530

    useState()

--- React_state/src/counter.jsx
+++ React_state/src/counter.jsx
@@ -1,14 +1,25 @@
-export default function Counter(){
-    let count = 0;
+import {useState} from "react";
 
-    function incCount(){
-        count += 1;
-        console.log(count);
-    }
+export default function Counter() {
+
+    let[count,setCount] = useState(0);// let [stateVariable,setStateVariable]= useState(10);
+    
+   let incCount = () => {
+    setCount(count+1);
+    console.log(count);
+   };
+  
+//export default function Counter(){
+    //let count = 0;
+
+    //function incCount(){
+      //  count += 1;
+    //    console.log(count);
+  //  }
     return (
         <div>
             <h3>Count = {count} </h3>
-            <button onClick={incCount}>Increse Count</button>
+            <button onClick={incCount}>Increase Count</button>
         </div>
     );
 }
